cf5c9ceb44b47b87fa6801de824ec8a3f81fabe9
Alex Mandel <alex@developmentseed.org>
2023-05-18 23:55:51 +0000

    chore: forgot to add asset_utils, PREVIEW not PREVIEWS

---
+++ src/stactools/cop_dem/assets_utils.py
@@ -0,0 +1,16 @@
+"""
+Creates the Item Assets section
+"""
+import os.path
+
+
+def change_asset_directory(href: str, asset_name: str,
+                           subdirectory: str) -> tuple[str, str]:
+    """
+    Apply a subdirectory prefix
+    """
+    asset_href = os.path.join(os.path.dirname(href), subdirectory,
+                              os.path.basename(href))
+    asset_href = asset_href.replace("DEM.tif", asset_name)
+
+    return (asset_name[:-4], asset_href)
